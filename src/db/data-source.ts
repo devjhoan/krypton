69e1957777358a17793b95be602f2e11b77178be
DevJhoan <znotjhoan@gmail.com>
2024-08-23 21:21:15 -0500

    feat(db): :wrench: add a better way to configure multiple databases

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,14 +1,27 @@
+import { DataSource, type DataSourceOptions } from "typeorm";
 import { Giveaway } from "@/db/entity/Giveaway";
 import { config } from "@/utils/getConfigFiles";
 import { Guild } from "@/db/entity/Guild";
 import { Ticket } from "./entity/Ticket";
 import { User } from "@/db/entity/User";
-import { DataSource } from "typeorm";
+
+const sqliteConfiguration: DataSourceOptions = {
+	type: "sqlite",
+	database: config.DatabaseSettings.SQLitePath,
+};
+
+const mongodbConfiguration: DataSourceOptions = {
+	type: "mongodb",
+	url: config.DatabaseSettings.MongoUri,
+};
+
+const appSourceConfiguration = {
+	sqlite: sqliteConfiguration,
+	mongodb: mongodbConfiguration,
+};
 
 export const AppDataSource = new DataSource({
-	type: config.DatabaseSettings.Type,
-	database: config.DatabaseSettings.SQLitePath,
-	url: config.DatabaseSettings.MongoUri,
+	...appSourceConfiguration[config.DatabaseSettings.Type],
 	synchronize: true,
 	logging: false,
 	entities: [Guild, Giveaway, User, Ticket],
